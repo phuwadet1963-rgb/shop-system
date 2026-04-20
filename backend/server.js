80a7177f47e91a36cff2cb393a39300f4dd2f464
phuwadet1963-rgb <phuwadet1963@gmail.com>
2026-04-21 01:37:32 +0700

    11

--- backend/server.js
+++ backend/server.js
@@ -39,14 +39,14 @@ const upload = multer({ storage: storage });
 
 // เปลี่ยนจากค่าเดิมที่เป็นข้อความตรงๆ เป็น process.env.ชื่อตัวแปร
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    ssl: {
-        rejectUnauthorized: false
-    }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
